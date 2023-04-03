e6d47f4b5a6548809cb178340f84bc379e48a9fb
MoonInHo <ansdlsgh55@gmail.com>
2023-04-03 22:58:51 +0900

    회원가입 기능 tdd기반 개발 피드백반영 완료

--- src/test/java/com/mooninho/starbucks/v2/service/MemberServiceTest.java
+++ src/test/java/com/mooninho/starbucks/v2/service/MemberServiceTest.java
@@ -1,7 +1,7 @@
 package com.mooninho.starbucks.v2.service;
 
-import com.mooninho.starbucks.v2.dto.MemberDto;
 import com.mooninho.starbucks.v2.dto.MemberJoinDto;
+import com.mooninho.starbucks.v2.entity.Member;
 import com.mooninho.starbucks.v2.exception.UserException;
 import com.mooninho.starbucks.v2.repository.MemberQueryRepository;
 import com.mooninho.starbucks.v2.repository.MemberRepository;
@@ -13,39 +13,36 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.mock;
-
+import static org.mockito.Mockito.*;
 
 @DisplayName("[유닛 테스트] - 회원 서비스")
 public class MemberServiceTest {
 
     private final MemberRepository memberRepository = mock(MemberRepository.class);
     private final MemberQueryRepository memberQueryRepository = mock(MemberQueryRepository.class);
-    private final MemberService memberServiceMock = mock(MemberService.class);
     private final MemberService memberService = new MemberService(memberRepository, memberQueryRepository);
-
-    //TODO MemberService 클래스를 Mock객체로 만들어야 첫번째 테스트가 성공하는데 그 이유
 
     @Test
     @DisplayName("회원가입 - 올바른 회원정보로 가입시 회원생성")
     void memberInfo_join_createMember() {
         //given
-        MemberDto memberDto = MemberDtoFixture.createMemberDto();
-        MemberJoinDto memberJoinDto = MemberJoinDto.setMemberInfo(memberDto);
+        MemberJoinDto memberJoinDto = MemberDtoFixture.getMemberJoinDto();
+        Member member = memberJoinDto.toMember();
+        given(memberRepository.save(any()))
+                .willReturn(member);
 
-        //when
-        Long memberId = memberServiceMock.signUp(memberJoinDto);
+        // when
+        Member savedMember = memberRepository.save(member);
 
         //then
-        assertThat(memberId).isNotNull();
+        assertThat(savedMember).isInstanceOf(Member.class);
     }
 
     @Test
     @DisplayName("회원 가입 - 이메일 중복시 예외 발생")
     void existsEmail_join_throwException() {
         //given
-        MemberDto memberDto = MemberDtoFixture.createMemberDto();
-        MemberJoinDto memberJoinDto = MemberJoinDto.setMemberInfo(memberDto);
+        MemberJoinDto memberJoinDto = MemberDtoFixture.getMemberJoinDto();
         given(memberQueryRepository.isEmailExist(any()))
                 .willReturn(true);
 
--- src/test/java/com/mooninho/starbucks/v2/support/fixture/MemberDtoFixture.java
+++ src/test/java/com/mooninho/starbucks/v2/support/fixture/MemberDtoFixture.java
@@ -1,8 +1,15 @@
 package com.mooninho.starbucks.v2.support.fixture;
 
 import com.mooninho.starbucks.v2.dto.MemberDto;
+import com.mooninho.starbucks.v2.dto.MemberJoinDto;
 
 public class MemberDtoFixture {
+
+    public static MemberJoinDto getMemberJoinDto() {
+        MemberDto memberDto = MemberDtoFixture.createMemberDto();
+        MemberJoinDto memberJoinDto = MemberJoinDto.setMemberInfo(memberDto);
+        return memberJoinDto;
+    }
 
     public static MemberDto createMemberDto(String email, String password) {
         MemberDto memberDto = new MemberDto();
